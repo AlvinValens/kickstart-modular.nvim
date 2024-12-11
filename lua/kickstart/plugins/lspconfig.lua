5ea86d1d9e8c6ba310cb89cdfc4fb2eb2e22fff8
Alvin Valenzuela <alvin.vlnz14@gmail.com>
2024-12-11 14:29:11 -0800

    Changed

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -178,7 +178,21 @@ return {
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
+        gopls = {},
+        css_variables = {},
+        cssls = {},
+        cssmodules_ls = {},
+        grammarly = {},
+        html = {},
+        htmx = {},
+        jsonls = {},
+        nextls = {},
+        phpactor = {},
+        sqlls = {},
+        svelte = {},
+        ts_ls = {},
+        zls = {},
+        yamlls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
